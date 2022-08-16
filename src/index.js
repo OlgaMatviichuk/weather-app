18f986004ed4043c25063f4b0f0ac821e94a589b
OlgaMatviichuk <109869771+OlgaMatviichuk@users.noreply.github.com>
2022-08-16 21:15:00 +0300

    Set function to get coordinates

--- src/index.js
+++ src/index.js
@@ -76,6 +76,13 @@ function displayForecast() {
   console.log(forecastHTML);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "57095abf8ac84e0b68c9e58e564131570";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
 
@@ -95,6 +102,8 @@ function showWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].main);
+
+  getForecast(response.data.coord);
 }
 
 function apiSearch(city) {
