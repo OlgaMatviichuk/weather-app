a1c7d8d1dee098a235b89029c76ce1c5df37bb09
OlgaMatviichuk <109869771+OlgaMatviichuk@users.noreply.github.com>
2022-08-16 20:46:08 +0300

    Dummy forecast transfered from HTML to JS

--- src/index.js
+++ src/index.js
@@ -46,6 +46,36 @@ function showTime() {
 let currentTime = document.querySelector("#time");
 currentTime.innerHTML = showTime();
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function showWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
 
@@ -108,3 +138,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelcius);
 
 apiSearch("New York");
+displayForecast();
